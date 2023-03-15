07d34bcd6058bc0462eb0a3480f152a4a24aa6f6
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-03-15 14:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+const age = document.querySelector('#age');
+const fullName = document.querySelector('#name');
+const btn = document.querySelector('#btn');
+
+
+// creating of promise
+const prom = new Promise((resolve, reject) => {
+	if(age >= 18) {
+		resolve(`Welcome, ${fullName}. You can vote`);
+	}
+	else {
+		reject(`Oh sorry ${fullName}, You aren't old enough`);
+	}
+})
+
+btn.addEventListener('click', function() {
+	e.preventDefault();
+	setTimeout(() => {
+		prom.then(() => {
+			alert(data);
+		})
+		.catch(() => {
+			alert(data);
+		});
+	}, 4000);
+	
+})
