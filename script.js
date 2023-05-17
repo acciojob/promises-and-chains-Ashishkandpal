4f2991d44d0331c62cfe5dabb9c9af5c399acc73
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-17 14:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-"use strict";
-
-//your JS code here. If required.
-
-// const ageElt = document.querySelector("#age");
-// const fullNameElt = document.querySelector("#name");
-// const btn = document.querySelector("#btn");
-
-// //creating function
-// const checkAge = function () {
-//   const prom = new Promise((resolve, reject) => {
-//     const age = ageElt.value;
-//     const fullName = fullNameElt.value;
-//     if (age >= 18) {
-//       resolve(`Welcome, ${fullName}. You can vote`);
-//     } else {
-//       reject(`Oh sorry ${fullName}, You aren't old enough`);
-//     }
-//   });
-
-//   setTimeout(() => {
-//     prom
-//       .then((data) => {
-//         alert(data);
-//       })
-//       .catch((err) => {
-//         alert(err);
-//       });
-//   }, 4000);
-// };
-
 function check(){
 	let age = document.getElementById("age").value;
 	let name = document.getElementById("name").value;
@@ -44,7 +13,6 @@ function check(){
 	})
 }
 
-// creating of promise
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
